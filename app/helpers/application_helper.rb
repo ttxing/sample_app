443476a5473af95a63b7195c335cfee67a18b88b
zhangmengzhi <104446930@qq.com>
2015-09-17 11:22:00 +0800

    Tittle Help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_tittle page_tittle=''
+    base_tittle = 'Sample App'
+    if page_tittle.empty?
+      base_tittle
+    else
+      "#{page_tittle} | #{base_tittle}"
+    end
+  end
 end
