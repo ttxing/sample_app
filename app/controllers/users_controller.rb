b8be98a8ecdcbdc85a54974e6b6badfa2e5d1f8a
zhangmengzhi <104446930@qq.com>
2015-09-23 10:55:31 +0800

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   def new
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @micropost = @user.microposts.build if logged_in?
     @microposts = @user.microposts.paginate(page: params[:page], per_page: 10)
   # debugger
   end
@@ -25,7 +24,7 @@ class UsersController < ApplicationController
       # flash[:success] = "Welcome to the Sample App!"
       # redirect_to user_url(@user)
       # redirect_to @user
-      
+
       # UserMailer.account_activation(@user).deliver_now
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
@@ -55,13 +54,27 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page], per_page: 10)
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page], per_page: 10)
+    render 'show_follow'
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
- # def logged_in_user
+  # def logged_in_user
   # unless logged_in?
   # store_location
   # flash[:danger] = "Please log in."
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name: "Example User",
             email: "example@railstutorial.org",
-            password: "foobar",
-            password_confirmation: "foobar",
+            password: "password",
+            password_confirmation: "password",
             admin: true,
             activated: true,
             activated_at: Time.zone.now)
@@ -30,3 +30,11 @@ users = User.order(:created_at).take(6)
 content = Faker::Lorem.sentence(5)
 users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
